c40c0ad6e81fff7b1e3e20f58a7b568cb64d395a
TomasCarvalho <2606tom@gmail.com>
2023-03-22 17:07:23 +0000

    Create PowersOf2UntilN Method

--- Power2Method/Program.cs
+++ Power2Method/Program.cs
@@ -4,6 +4,15 @@ namespace Power2Method
 {
     class Program
     {
+        private static void PowersOf2UntilN(int n)
+        {
+            for (int i = 1 ; i <= ( 1 << n ) ; i<<=1 )
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+
         private static void PowersOf2Until5()
         {
             for (int i = 1 ; i <= ( 1 << 5 ) ; i<<=1 )
@@ -13,6 +22,14 @@ namespace Power2Method
         }
         static void Main(string[] args)
         {
+            string aux = Console.ReadLine();
+            int nCount = Convert.ToInt32(aux);
+            PowersOf2UntilN(nCount);
+
+            aux = Console.ReadLine();
+            nCount = Convert.ToInt32(aux);
+            PowersOf2UntilN(nCount);
+
             PowersOf2Until5();
             PowersOf2Until5();
         }
