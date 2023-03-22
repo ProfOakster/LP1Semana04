c82713b75c5b8cb8648e7b4d0f9d4412969710ef
TomasCarvalho <2606tom@gmail.com>
2023-03-22 21:40:11 +0000

    Implement Lucas program with console arguments

---
+++ Lucas/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Lucas
+{
+    class Program
+    {
+        private static int Lucas(int n)
+        {
+            int res;
+            if (n==0)
+                res = 2;
+            else if (n==1)
+                res = 1;
+            else
+                res = Lucas(n - 2) + Lucas(n - 1);
+
+            return res;
+        }
+
+        static void Main(string[] args)
+        {
+            int inp = int.Parse(args[0]);
+            Console.WriteLine(Lucas(inp));
+        }
+    }
+}
